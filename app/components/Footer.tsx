7b1005e765062d676942b73751d056bea3cee09d
marwan alrawshdeh <marwansalem55@yahoo.com>
2026-03-28 21:22:24 +0300

    chore: update copyright name and comment out LinkedIn link in Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -111,16 +111,16 @@ function MinimalFooter() {
         {/* Bottom Bar Segment */}
         <div className="border-t border-gray-100 flex flex-col md:flex-row justify-between items-center pt-8 gap-4">
           <p className="text-xs text-gray-400">
-            &copy; {new Date().getFullYear()} STAP. All rights reserved.
+            &copy; {new Date().getFullYear()} Science Community Publisher. All rights reserved.
           </p>
           
           <div className="flex items-center gap-6">
             <Link href={"/privacy-statement"} className="text-xs text-gray-400 hover:text-primary transition">Privacy Policy</Link>
-            <a aria-label="LinkedIn" href="https://www.linkedin.com/in/journal-of-cyber-security-and-risk-auditing-jcaiqc-b902b5349/" className="text-gray-400 hover:text-primary transition">
+            {/* <a aria-label="LinkedIn" href="https://www.linkedin.com/in/journal-of-cyber-security-and-risk-auditing-jcaiqc-b902b5349/" className="text-gray-400 hover:text-primary transition">
               <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 fill-current" viewBox="0 0 24 24">
                 <path fillRule="evenodd" d="M21 5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V5zm-2.5 8.2v5.3h-2.79v-4.93a1.4 1.4 0 0 0-1.4-1.4c-.77 0-1.39.63-1.39 1.4v4.93h-2.79v-8.37h2.79v1.11c.48-.78 1.47-1.3 2.32-1.3 1.8 0 3.26 1.46 3.26 3.26zM6.88 8.56a1.686 1.686 0 0 0 0-3.37 1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68zm1.39 1.57v8.37H5.5v-8.37h2.77z" clipRule="evenodd" />
               </svg>
-            </a>
+            </a> */}
           </div>
         </div>
         
